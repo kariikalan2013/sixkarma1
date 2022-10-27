8840f501fb50cb43a584eed72cf871677df19d2b
kariikalan2013 <114467276+kariikalan2013@users.noreply.github.com>
2022-10-27 14:01:58 +0530

    Add files via upload

---
+++ new project1.js
@@ -0,0 +1,15 @@
+  
+   function myfunction(){
+  let username= document.getElementById("example1").value;
+    let password=document.getElementById("example2").value;
+    if(username=="karikalan" &&password=="tamil")
+    {
+    
+    alert("Registered");
+    }
+    else{
+        alert("failed");
+    }
+
+
+}
